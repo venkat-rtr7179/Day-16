67653bf48f9711a7dabaadcff076ce3e57af4047
venkat-rtr7179 <125110848+venkat-rtr7179@users.noreply.github.com>
2023-06-08 10:11:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById('cntdwn').innerHTML = 10;
+    setTimeout(() => {
+        document.getElementById('cntdwn').innerHTML = 9;
+        setTimeout(() => {
+            document.getElementById('cntdwn').innerHTML = 8;
+            setTimeout(() => {
+                document.getElementById('cntdwn').innerHTML = 7;
+                setTimeout(() => {
+                    document.getElementById('cntdwn').innerHTML = 6;
+                    setTimeout(() => {
+                        document.getElementById('cntdwn').innerHTML = 5;
+                        setTimeout(() => {
+                            document.getElementById('cntdwn').innerHTML = 4;
+                            setTimeout(() => {
+                                document.getElementById('cntdwn').innerHTML = 3;
+                                setTimeout(() => {
+                                    document.getElementById('cntdwn').innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.getElementById('cntdwn').innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.getElementById('cntdwn').innerHTML = 'Happy New Year';
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
